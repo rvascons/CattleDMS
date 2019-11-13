e186420f60fc2c920f0aecd358bbf8213f9e348e
Rafael Vasconcelos <rvascons@gmail.com>
2019-11-13 16:21:33 -0300

    Add files via upload

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,10 +1,15 @@
 const cowDatabase = {};
-var cowCount = 0;
-var inputElement = document.getElementById("in01");
-var buttonElement = document.getElementById("bt01");
+var set = false;
+var input01Element = document.getElementById("in01");
+var button01Element = document.getElementById("bt01");
+var button03Element = document.getElementById("bt03");
+var input02Element = document.getElementById("in02");
+var button02Element = document.getElementById("bt02");
+var button04Element = document.getElementById("bt04");
+var tableElement = document.getElementById("table");
 
 (function(){
-    function newCollar(id,x,y){
+    function newCollar(id){
         const collar = {
             id : id,
             pos: [0,0],
@@ -12,7 +17,7 @@ var buttonElement = document.getElementById("bt01");
             open : false,
             time: firebase.database.ServerValue.TIMESTAMP,
         };
-        cowCount ++;
+        mapsControl.setMarker(0,0,id,2);
         let changes = {};
         changes['/Collars/' + collar.id] = collar;
         let collarRef = firebase.database().ref();
@@ -23,11 +28,38 @@ var buttonElement = document.getElementById("bt01");
             .catch(function(error){
                 return {success : false, msg : `Error to register new collar = ${error.message}`};
             })
-    }   
-    function newRequest(id){
+    }
+    function rmvCollar(id){
+        let collarRef = firebase.database().ref('/Collars/' + id);
+        mapsControl.rmvMarker(id);
+        collarRef.remove()
+            .then(function(){
+                return {success : true, msg: 'Collar removed'};
+            })
+            .catch(function(error){
+                return {success : false, msg: `Error to remove collar = ${error.message}`};
+            })     
+    } 
+    function openRequest(id){
         const request = {
             id : id,
-            state : false,   
+            status : 'unlock',   
+        }
+        let collarRef = firebase.database().ref();
+        let requests = {};
+        requests['/Request/' + id] = request;
+        collarRef.update(requests)
+            .then(function(){
+                return {success : true, msg : 'Collar Requested'};
+            })
+            .catch(function(error){
+                return {success : false, msg : `${error.message}`};
+            })
+    }
+    function lockRequest(id){
+        const request = {
+            id : id,
+            status : 'lock',   
         }
         let collarRef = firebase.database().ref();
         let requests = {};
@@ -41,45 +73,131 @@ var buttonElement = document.getElementById("bt01");
             })
     }
     function newFixPoint(id, x, y){
-        const collar = {
+        const fixPoint = {
             id : id,
             pos: [x,y],
+            fence : true,
             time: firebase.database.ServerValue.TIMESTAMP,
         };
+
+        if(id == 0){
+          mapsControl.setMarker(0,0,0,0);
+        }
+        if(id == 1){
+          mapsControl.setMarker(x,y,1,3);
+        }
+        if(id == 2){
+          mapsControl.setMarker(x,y,2,1);
+        }
+        
+        let changes = {};
+        changes['/FixPoint/' + fixPoint.id] = fixPoint;
+        let fixPointRef = firebase.database().ref();
+        fixPointRef.update(changes)
+            .then(function(){
+                return {success : true, msg : 'New collar registered'};
+            })
+            .catch(function(error){
+                return {success : false, msg : `Error to register new collar = ${error.message}`};
+            })
     }
     function rmvFixPoint(id){
         let collarRef = firebase.database().ref('/Request/' + id);
+        mapsControl.rmvMarker(id);
         collarRef.remove()
             .then(function(){
-                cowCount --;
                 return {success : true, msg: 'FixPoint removed'};
             })
             .catch(function(error){
                 return {success : false, msg: `Error to remove FixPoint = ${error.message}`};
             })     
     }
-    function rmvCollar(id){
-        let collarRef = firebase.database().ref('/Collars/' + id);
-        collarRef.remove()
-            .then(function(){
-                cowCount --;
-                return {success : true, msg: 'Collar removed'};
-            })
-            .catch(function(error){
-                return {success : false, msg: `Error to remove collar = ${error.message}`};
-            })     
-    }
     function shwCollar(id){
-        var collarIdRef = firebase.database().ref('/Collars/' + id +'/pos');
+        var collarIdRef = firebase.database().ref('/Collars/' + id);
         collarIdRef.on('value', function(snapshot) {
-            console.log(snapshot.val());
+            var data = snapshot.val();
+            console.log(data.id);
+            alert('Posição: ' + data.pos);
         });
     }
-    cowDatabase.newRequest = newRequest;
+    function updateTable(){
+        var table = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+        var collarsRef = firebase.database().ref("/Collars/");
+        collarsRef.on('value', function(snapshot){
+            table.innerHTML = '';
+            snapshot.forEach(function(childSnapshot){
+                var data = childSnapshot.val();
+                var index = data.id;
+                var row = table.insertRow(-1);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
+                cell1.innerHTML = data.id;
+                cell2.innerHTML = data.open;
+                cell3.innerHTML = data.pos;
+                cell4.innerHTML = data.last_pos;
+            });     
+        });
+    }
+    function requestReply(){
+        var collarReqRef = firebase.database().ref('/Request/');
+        collarReqRef.on('value', function(snapshot){
+          snapshot.forEach(function(childSnapshot){
+            var data = childSnapshot.val();
+            if(data.status == 'ok1'){
+                alert('Coleira ' + data.id + ' desbloqueada.');
+                var collarIdRef = firebase.database().ref('/Collars/' + data.id);
+                collarIdRef.child('open').set(true);
+            }else if(data.status == 'ok2'){
+                alert('Coleira ' + data.id + ' bloqueada.');
+                var collarIdRef = firebase.database().ref('/Collars/' + data.id);
+                collarIdRef.child('open').set(false);
+            }
+          });
+        });
+    }
+    button01Element.onclick = function unlockCow(){
+        console.log('botao de liberação clicado');
+        var id = input01Element.value;
+        input01Element.value = '';
+        openRequest(id);
+    }
+    button02Element.onclick = function findCow(){
+        console.log('botao de busca clicado');
+        var id = input02Element.value;
+        input02Element.value = '';
+        shwCollar(id);
+    }
+    button03Element.onclick = function lockCow(){
+        console.log('botao de bloquei clicado');
+        var id = input01Element.value;
+        input01Element.value = '';
+        lockRequest(id);
+    }
+    button04Element.onclick = function Fence(){
+        if(set == false){
+            set = true;
+            mapsControl.showFence(0);
+        }else{
+            set = false;
+            mapsControl.showFence(1);
+        }
+    }
+    
+    cowDatabase.requestReply = requestReply;
+    cowDatabase.updateTable = updateTable;
+    cowDatabase.rmvFixPoint = rmvFixPoint;
+    cowDatabase.newFixPoint = newFixPoint;
+    cowDatabase.lockRequest = lockRequest;
+    cowDatabase.openRequest = openRequest;
     cowDatabase.newCollar = newCollar;
     cowDatabase.rmvCollar = rmvCollar;
     cowDatabase.shwCollar = shwCollar;
-})()
+})();
+
+
+
 
 
 
--- Project/js/maps.js
+++ Project/js/maps.js
@@ -1,14 +1,17 @@
 const mapsControl = {};
+const myLng = -34.95193;
+const myLat = -8.05517;
+var map;
+var markers = [];
+var markersCount = 0;
+var labels = ['Gateway','FixPoint','Cow','EVAL'];
+var fenceEdge;
 
 (function(){
-  var map;
-  var markers = [];
-  var markersCount = 0;
-  var labels = ['Gateway','FixPoint','Cow','EVAL'];
   function createMap(){
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -12.77, lng: -47.92},
-      zoom: 4,
+      center: {lat: -8.1, lng: -34.9},
+      zoom: 11,
       styles: [
         {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
         {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
@@ -92,23 +95,171 @@ const mapsControl = {};
     })
   }
   function setMarker(x,y,index,kind){
-    markers[index] = new google.maps.Marker({
-      position: {lat: x, lng: y},
-      map: map,
-      label: labels[kind] 
-    });
+    if(kind == 1 || kind == 3){
+      var new_longitude = myLng + (x / 6371000) * (180 / 3.1415926) / Math.cos(myLng * 3.1415926/180);
+      var new_latitude  = myLat  + (y / 6371000) * (180 / 3.1415926);
+      markers[index] = new google.maps.Marker({
+        position: {lat: new_latitude, lng: new_longitude},
+        map: map,
+        label: labels[kind],
+      });
+    }else if(kind == 2){
+      var new_longitude = myLng + (x / 6371000) * (180 / 3.1415926) / Math.cos(myLng * 3.1415926/180);
+      var new_latitude  = myLat  + (y / 6371000) * (180 / 3.1415926);
+      markers[index] = new google.maps.Marker({
+        position: {lat: new_latitude, lng: new_longitude},
+        map: map,
+        icon: {
+          url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+        }
+      });
+    }else{
+      markers[index] = new google.maps.Marker({
+        position: {lat: myLat, lng: myLng},
+        map: map,
+        label: labels[kind],
+        
+      });
+    }
+
     markersCount++;
   }
   function rmvMarker(index){
+    markersCount--;
     markers[index].setMap(null);
   }
+  function moveMarker(dx,dy,id){
+    var new_longitude = myLng + (dx / 6371000) * (180 / 3.1415926) / Math.cos(myLng * 3.1415926/180);
+    var new_latitude  = myLat  + (dy / 6371000) * (180 / 3.1415926);
+    var latlng = new google.maps.LatLng(new_latitude, new_longitude);
+    markers[id].setPosition(latlng);
 
-  //new_latitude  = latitude  + (dy / r_earth) * (180 / pi);
-  //new_longitude = longitude + (dx / r_earth) * (180 / pi) / cos(latitude * pi/180);
-
-
+  }
+  function updateMapCollars(){
+    var collarsRef = firebase.database().ref("/Collars/");
+    collarsRef.on('value', function(snapshot){
+      if(snapshot.hasChildren()){
+        snapshot.forEach(function(childSnapshot){
+          var data = childSnapshot.val();
+          var x = data.pos[0];
+          var y = data.pos[1];
+          moveMarker(x,y,data.id);
+          checkIn(data.id);
+        });
+      }
+    });
+  }
+  function updateMapFixPoints(){
+    var collarsRef = firebase.database().ref("/FixPoint/");
+    collarsRef.on('value', function(snapshot){
+      if(snapshot.hasChildren()){
+      snapshot.forEach(function(childSnapshot){
+        var data = childSnapshot.val();
+        var x = data.pos[0];
+        var y = data.pos[1];
+        moveMarker(x,y,data.id);
+      });
+      }
+    });
+  }
+  function checkForCollars(){
+    var collarsRef = firebase.database().ref("/Collars/");
+    collarsRef.once('value', function(snapshot){
+      snapshot.forEach(function(childSnapshot){
+        var data = childSnapshot.val();
+        var x = data.pos[0];
+        var y = data.pos[1];
+        setMarker(x,y,data.id,2);
+      });
+      updateMapCollars();
+    });
+  }
+  function checkForFixPoints(){
+    var collarsRef = firebase.database().ref("/FixPoint/");
+    collarsRef.once('value', function(snapshot){
+      snapshot.forEach(function(childSnapshot){
+        var data = childSnapshot.val();
+        var x = data.pos[0];
+        var y = data.pos[1];
+        if(data.id == 0){
+          setMarker(x,y,data.id,0);
+        }else if(data.id == 1){
+          setMarker(x,y,data.id,3);
+        }else{
+          setMarker(x,y,data.id,1);
+        }
+      })
+      createFence();
+    })
+    updateMapFixPoints();
+  }
+  function createFence(){
+    var collarsRef = firebase.database().ref("/FixPoint/");
+    var fenceLatLng = [];
+    collarsRef.once('value', function(snapshot){
+      if(snapshot.numChildren() > 2){
+        snapshot.forEach(function(childSnapshot){
+          var data = childSnapshot.val();
+          if(data.fence == true){
+            pos = markers[data.id].getPosition();
+            fenceLatLng.push(pos);
+          }
+        });
+        fenceEdge = new google.maps.Polygon({
+          paths: fenceLatLng,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 0.9,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35
+        });
+        fenceEdge.setMap(map);
+      }
+      checkAll();
+    })
+  }
+  function showFence(status){
+    if(status == 1){
+      fenceEdge.setVisible(true);
+    }else if(status == 0){
+      fenceEdge.setVisible(false);
+    }
+  }
+  function checkAll(){
+    var collarsRef = firebase.database().ref("/Collars/");
+    collarsRef.once('value', function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+          var data = childSnapshot.val();
+          var pos = markers[data.id].getPosition();
+          if(google.maps.geometry.poly.containsLocation(pos, fenceEdge) == false){
+            alert('Vaca ' + data.id + ' fora da cerca. Ultima posicao marcada: ' + data.last_pos);
+          }
+        });
+    });
+  }
+  function checkIn(id){
+    var collarsRef = firebase.database().ref("/Collars/" + id);
+    collarsRef.once('value', function(snapshot){
+      if(snapshot.hasChildren()){
+        var data = snapshot.val();
+        var pos = markers[data.id].getPosition();
+        if(google.maps.geometry.poly.containsLocation(pos, fenceEdge) == false){
+          alert('Vaca ' + data.id + ' fora da cerca. Ultima posicao marcada: ' + data.last_pos);
+        }
+      }
+    });
+  }
+  mapsControl.checkAll = checkAll;
+  mapsControl.checkIn = checkIn;
+  mapsControl.showFence = showFence;
+  mapsControl.createFence = createFence;
+  mapsControl.checkForCollars = checkForCollars;
+  mapsControl.checkForFixPoints = checkForFixPoints;
+  mapsControl.updateMapFixPoints = updateMapFixPoints;
+  mapsControl.updateMapCollars = updateMapCollars;
   mapsControl.rmvMarker = rmvMarker;
   mapsControl.setMarker = setMarker;
   mapsControl.createMap = createMap;
+  mapsControl.moveMarker = moveMarker;
 })()
-      
+    
